9bd7c0bd48060beced2b84a2a64c463ffb0ca2cf
aleksshru <aleksshru@gmail.com>
2021-02-12 13:10:05 +0300

    one1

---
+++ task8compilation/task7_func.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include "func.h"
+using namespace std;
+double x, result;
+void f(void)
+{
+    result = pow(cos(x), 2) - pow(sin(x), 2);
+}
---
+++ task8compilation/task7_main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "func.h"
+using namespace std;
+int main()
+{
+    x = 2;
+    cout << "x = " << x << endl;
+    f();
+    cout << "y = " << fixed << setprecision(4) << result << endl;
+    cout << "Enter x ";
+    cin >> x;
+    f();
+    cout << "y = " << fixed << setprecision(4) << result;
+    return 0;
+}
